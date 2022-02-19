bf612eba848e9a2dcdaacd15e6fa0387737602d8
Eduardo Millán <eduardo.millan.saavedra@gmail.com>
2022-02-18 22:49:03 -0600

    add json settings

--- lua/nv/core/cmds.lua
+++ lua/nv/core/cmds.lua
@@ -9,9 +9,11 @@ local command = utils.command
 autocmd("TextYankPost", "*", 'silent! lua vim.highlight.on_yank{higroup="Visual", timeout=250}')
 autocmd("FileType", "qf", "nnoremap <silent> <buffer> <CR> <CR>:cclose<CR>")
 autocmd("BufWritePre", "*", 'lua require("nv.utils").format()')
+autocmd("BufRead,BufNewFile", "tsconfig.json", "setlocal filetype=jsonc")
 
 -- *******************************
 -- Commands
 -- *******************************
+command("Terminal", "vnew | set norelativenumber | terminal")
 command("Format", 'lua require("nv.utils").format()')
 command("Sync", "PackerSync")
