9948f6b9e8d5bcc71fd6849f2d94a0e2284054ae
Eduardo Millán <eduardo.millan.saavedra@gmail.com>
2022-05-13 00:08:48 -0500

    refactor autocmd from new lua api

--- lua/nv/core/cmds.lua
+++ lua/nv/core/cmds.lua
@@ -1,25 +1,19 @@
-local utils = require("nv.utils")
-
-local command = utils.command
-
 local autocmd = vim.api.nvim_create_autocmd
 local augroup = vim.api.nvim_create_augroup
+local command = vim.api.nvim_create_user_command
 local cmd = vim.cmd
 
 -- *******************************
 -- Commands
 -- *******************************
-command("Terminal", "new | set norelativenumber | terminal")
-command("VTerminal", "vnew | set norelativenumber | terminal")
-command("TTerminal", "tabnew | set norelativenumber | terminal")
-
-command("Format", 'lua require("nv.utils").format()')
-command("Sync", 'lua require("nv.utils").sync()')
+command("Sync", function()
+  cmd(("source %s/lua/nv/plugins.lua | PackerSync"):format(vim.g.nv.dir.nvim.config))
+end, {})
 
 -- *******************************
 -- Autocmds
 -- *******************************
-local group = augroup("MyAutocmds", { clear = true })
+local group = augroup("NVAutocmds", { clear = true })
 
 autocmd(
   "TextYankPost",
@@ -31,20 +25,7 @@ autocmd("FileType", { pattern = "qf", command = "nnoremap <silent> <buffer> <CR>
 autocmd("BufWritePre", {
   pattern = "*",
   callback = function()
-    local clients = vim.tbl_filter(function(client)
-      return client.supports_method("textDocument/formatting")
-    end, vim.lsp.buf_get_clients())
-
-    if #clients > 0 then
-      cmd("silent lua vim.lsp.buf.formatting_sync({}, 1000)")
-      return
-    end
-
-    local pos = vim.fn.getpos(".")
-    cmd("normal gg=G")
-    vim.fn.setpos(".", pos)
-    cmd("normal zz")
-    cmd("silent! write")
+    vim.lsp.buf.formatting_sync({}, 1000)
   end,
   group = group,
 })
